8b69de9faad48ad2db2e67a748ca8aeb4c2814bb
Kabir Sarin <sarink87@gmail.com>
2020-10-12 12:58:35 -0600

    Run build

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,29 @@
 import * as React from "react";
 declare type RequestIdleCallbackHandle = number;
 declare type RequestIdleCallbackOptions = {
-  timeout: number;
+    timeout: number;
 };
 declare type RequestIdleCallbackDeadline = {
-  readonly didTimeout: boolean;
-  timeRemaining: () => number;
+    readonly didTimeout: boolean;
+    timeRemaining: () => number;
 };
 declare global {
-  interface Window {
-    requestIdleCallback?: (
-      callback: (deadline: RequestIdleCallbackDeadline) => void,
-      opts?: RequestIdleCallbackOptions
-    ) => RequestIdleCallbackHandle;
-    cancelIdleCallback?: (handle: RequestIdleCallbackHandle) => void;
-  }
+    interface Window {
+        requestIdleCallback?: (callback: (deadline: RequestIdleCallbackDeadline) => void, opts?: RequestIdleCallbackOptions) => RequestIdleCallbackHandle;
+        cancelIdleCallback?: (handle: RequestIdleCallbackHandle) => void;
+    }
 }
 export declare type LazyProps = {
-  ssrOnly?: boolean;
-  whenIdle?: boolean;
-  whenVisible?: boolean;
-  noWrapper?: boolean;
-  didHydrate?: VoidFunction;
-  promise?: Promise<any>;
-  on?: (keyof HTMLElementEventMap)[] | keyof HTMLElementEventMap;
-  listenOnEl?:
-    | HTMLElementTagNameMap[keyof HTMLElementTagNameMap]
-    | HTMLDocument
-    | Window;
+    ssrOnly?: boolean;
+    whenIdle?: boolean;
+    whenVisible?: boolean;
+    noWrapper?: boolean;
+    didHydrate?: VoidFunction;
+    promise?: Promise<any>;
+    on?: (keyof HTMLElementEventMap)[] | keyof HTMLElementEventMap;
+    listenOnEl?: HTMLElementTagNameMap[keyof HTMLElementTagNameMap] | HTMLDocument | Window;
 };
-declare type Props = Omit<
-  React.HTMLProps<HTMLDivElement>,
-  "dangerouslySetInnerHTML"
-> &
-  LazyProps;
+declare type Props = Omit<React.HTMLProps<HTMLDivElement>, "dangerouslySetInnerHTML"> & LazyProps;
 declare type VoidFunction = () => void;
 declare const LazyHydrate: React.FunctionComponent<Props>;
 export default LazyHydrate;
--- index.esm.js
+++ index.esm.js
@@ -1,210 +1,151 @@
-import _extends from "@babel/runtime/helpers/esm/extends";
-import _objectWithoutPropertiesLoose from "@babel/runtime/helpers/esm/objectWithoutPropertiesLoose";
-import {
-  createElement,
-  Fragment,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState
-} from "react";
+import _extends from '@babel/runtime/helpers/esm/extends';
+import _objectWithoutPropertiesLoose from '@babel/runtime/helpers/esm/objectWithoutPropertiesLoose';
+import { useRef, useState, useEffect, createElement, Fragment, useLayoutEffect } from 'react';
 
 var isBrowser = typeof document !== "undefined";
 
 var event = "hydrate";
-var io =
-  isBrowser && IntersectionObserver
-    ? new IntersectionObserver(
-        function(entries) {
-          entries.forEach(function(entry) {
-            if (entry.isIntersecting || entry.intersectionRatio > 0) {
-              entry.target.dispatchEvent(new CustomEvent(event));
-            }
-          });
-        },
-        {
-          rootMargin: "150px"
-        }
-      )
-    : null; // React currently throws a warning when using useLayoutEffect on the server.
+var io = ( isBrowser) && IntersectionObserver ? new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting || entry.intersectionRatio > 0) {
+      entry.target.dispatchEvent(new CustomEvent(event));
+    }
+  });
+}, {
+  rootMargin: "150px"
+}) : null; // React currently throws a warning when using useLayoutEffect on the server.
 
-var useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
+var useIsomorphicLayoutEffect =  isBrowser ? useLayoutEffect : useEffect;
 
 var LazyHydrate = function LazyHydrate(props) {
   var childRef = useRef(null); // Always render on server
 
-  var _React$useState = useState(!isBrowser),
-    hydrated = _React$useState[0],
-    setHydrated = _React$useState[1];
+  var _React$useState = useState(!( isBrowser)),
+      hydrated = _React$useState[0],
+      setHydrated = _React$useState[1];
 
   var noWrapper = props.noWrapper,
-    ssrOnly = props.ssrOnly,
-    whenIdle = props.whenIdle,
-    whenVisible = props.whenVisible,
-    promise = props.promise,
-    _props$on = props.on,
-    on = _props$on === void 0 ? [] : _props$on,
-    children = props.children,
-    didHydrate = props.didHydrate,
-    listenOnEl = props.listenOnEl,
-    rest = _objectWithoutPropertiesLoose(props, [
-      "noWrapper",
-      "ssrOnly",
-      "whenIdle",
-      "whenVisible",
-      "promise",
-      "on",
-      "children",
-      "didHydrate",
-      "listenOnEl"
-    ]);
+      ssrOnly = props.ssrOnly,
+      whenIdle = props.whenIdle,
+      whenVisible = props.whenVisible,
+      promise = props.promise,
+      _props$on = props.on,
+      on = _props$on === void 0 ? [] : _props$on,
+      children = props.children,
+      didHydrate = props.didHydrate,
+      listenOnEl = props.listenOnEl,
+      rest = _objectWithoutPropertiesLoose(props, ["noWrapper", "ssrOnly", "whenIdle", "whenVisible", "promise", "on", "children", "didHydrate", "listenOnEl"]);
 
-  if (
-    "production" !== process.env.NODE_ENV &&
-    !ssrOnly &&
-    !whenIdle &&
-    !whenVisible &&
-    !on.length &&
-    !promise
-  ) {
-    console.error(
-      "LazyHydration: Enable atleast one trigger for hydration.\n" +
-        "If you don't want to hydrate, use ssrOnly"
-    );
+  if ('production' !== process.env.NODE_ENV && !ssrOnly && !whenIdle && !whenVisible && !on.length && !promise) {
+    console.error("LazyHydration: Enable atleast one trigger for hydration.\n" + "If you don't want to hydrate, use ssrOnly");
   }
 
-  useIsomorphicLayoutEffect(function() {
+  useIsomorphicLayoutEffect(function () {
     var _childRef$current;
 
     // No SSR Content
-    if (
-      !((_childRef$current = childRef.current) == null
-        ? void 0
-        : _childRef$current.hasChildNodes())
-    ) {
+    if (!((_childRef$current = childRef.current) == null ? void 0 : _childRef$current.hasChildNodes())) {
       setHydrated(true);
     }
   }, []);
-  useEffect(
-    function() {
-      if (ssrOnly || hydrated) return;
-      var cleanupFns = [];
+  useEffect(function () {
+    if (ssrOnly || hydrated) return;
+    var cleanupFns = [];
 
-      function cleanup() {
-        while (cleanupFns.length) {
-          cleanupFns.pop()();
-        }
+    function cleanup() {
+      while (cleanupFns.length) {
+        cleanupFns.pop()();
       }
+    }
 
-      function hydrate() {
-        setHydrated(true);
-        if (didHydrate) didHydrate();
-      }
+    function hydrate() {
+      setHydrated(true);
+      if (didHydrate) didHydrate();
+    }
 
-      if (promise) {
-        promise.then(hydrate)["catch"](hydrate);
-      }
+    if (promise) {
+      promise.then(hydrate)["catch"](hydrate);
+    }
 
-      if (whenIdle) {
-        if (window.requestIdleCallback) {
-          var idleCallbackId = window.requestIdleCallback(hydrate, {
-            timeout: 500
-          });
-          cleanupFns.push(function() {
-            if (window.cancelIdleCallback) {
-              window.cancelIdleCallback(idleCallbackId);
-            }
-          });
-        } else {
-          var id = setTimeout(hydrate, 2000);
-          cleanupFns.push(function() {
-            clearTimeout(id);
-          });
-        }
-      }
-
-      var events = Array.isArray(on) ? on.slice() : [on];
-
-      if (whenVisible) {
-        var _childRef$current2;
-
-        if (
-          io &&
-          ((_childRef$current2 = childRef.current) == null
-            ? void 0
-            : _childRef$current2.childElementCount)
-        ) {
-          // As root node does not have any box model, it cannot intersect.
-          var el = childRef.current.children[0];
-          io.observe(el);
-          events.push(event);
-          cleanupFns.push(function() {
-            io.unobserve(el);
-          });
-        } else {
-          return hydrate();
-        }
-      }
-
-      var elToListenOn = listenOnEl || childRef.current;
-
-      if (elToListenOn) {
-        events.forEach(function(event) {
-          elToListenOn.addEventListener(event, hydrate, {
-            once: true,
-            capture: true,
-            passive: true
-          });
-          cleanupFns.push(function() {
-            elToListenOn.removeEventListener(event, hydrate, {
-              capture: true
-            });
-          });
+    if (whenIdle) {
+      if (window.requestIdleCallback) {
+        var idleCallbackId = window.requestIdleCallback(hydrate, {
+          timeout: 500
+        });
+        cleanupFns.push(function () {
+          if (window.cancelIdleCallback) {
+            window.cancelIdleCallback(idleCallbackId);
+          }
+        });
+      } else {
+        var id = setTimeout(hydrate, 2000);
+        cleanupFns.push(function () {
+          clearTimeout(id);
         });
       }
+    }
 
-      return cleanup;
-    },
-    [hydrated, on, ssrOnly, whenIdle, whenVisible, didHydrate, promise]
-  );
+    var events = Array.isArray(on) ? on.slice() : [on];
+
+    if (whenVisible) {
+      var _childRef$current2;
+
+      if (io && ((_childRef$current2 = childRef.current) == null ? void 0 : _childRef$current2.childElementCount)) {
+        // As root node does not have any box model, it cannot intersect.
+        var el = childRef.current.children[0];
+        io.observe(el);
+        events.push(event);
+        cleanupFns.push(function () {
+          io.unobserve(el);
+        });
+      } else {
+        return hydrate();
+      }
+    }
+
+    var elToListenOn = listenOnEl || childRef.current;
+
+    if (elToListenOn) {
+      events.forEach(function (event) {
+        elToListenOn.addEventListener(event, hydrate, {
+          once: true,
+          capture: true,
+          passive: true
+        });
+        cleanupFns.push(function () {
+          elToListenOn.removeEventListener(event, hydrate, {
+            capture: true
+          });
+        });
+      });
+    }
+
+    return cleanup;
+  }, [hydrated, on, ssrOnly, whenIdle, whenVisible, didHydrate, promise]);
 
   if (hydrated) {
     if (noWrapper) {
-      return /*#__PURE__*/ createElement(Fragment, null, children);
+      return /*#__PURE__*/createElement(Fragment, null, children);
     }
 
-    return /*#__PURE__*/ createElement(
-      "div",
-      _extends(
-        {
-          ref: childRef,
-          style: {
-            display: "contents"
-          }
-        },
-        rest
-      ),
-      children
-    );
+    return /*#__PURE__*/createElement("div", _extends({
+      ref: childRef,
+      style: {
+        display: "contents"
+      }
+    }, rest), children);
   } else {
-    return /*#__PURE__*/ createElement(
-      "div",
-      _extends(
-        {
-          ref: childRef,
-          style: {
-            display: "contents"
-          },
-          suppressHydrationWarning: true
-        },
-        rest,
-        {
-          dangerouslySetInnerHTML: {
-            __html: ""
-          }
-        }
-      )
-    );
+    return /*#__PURE__*/createElement("div", _extends({
+      ref: childRef,
+      style: {
+        display: "contents"
+      },
+      suppressHydrationWarning: true
+    }, rest, {
+      dangerouslySetInnerHTML: {
+        __html: ""
+      }
+    }));
   }
 };
 
